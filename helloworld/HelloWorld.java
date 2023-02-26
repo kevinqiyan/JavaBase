916303ec4045f0eab943927d98665b9b2a8a2ad3
kevinqiyan <kevin1025253@gmail.com>
2023-02-26 16:43:19 +0800

    first

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+        double x, y, sum;
+        x = 1;
+        y = 2;
+        sum = x + y;
+        System.out.println(sum);
+        
+    }
+}
